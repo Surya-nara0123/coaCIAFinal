a9cd07a4e69c938ca27209c8eec348f953af7187
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-05-08 11:01:58 +0530

    done with extra work too

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,91 @@
 <script>
   import L1cache from "./l1cache.js";
-  import L2Cache from "./l2cache.js";
   import memory from "./mainMemory.js";
 
-  let mainMemory = new memory(64 * Math.pow(2, 10), 64);
-  let l1cache = new L1cache(64 * Math.pow(2, 10), 8 * Math.pow(2, 10), 128, 64);
-  let l2cache = new L2Cache(
-    64 * Math.pow(2, 10),
-    16 * Math.pow(2, 10),
-    256,
-    64,
-    4
-  );
+  let mainMemory = new memory(32 * 32 * 2, 32);
+  let l1cache = new L1cache(64 * 32, 32 * 64, 64, 32);
   let mainMemoryEnabled = false;
   let l1cacheEnabled = false;
-  let victimCacheEnabled = false;
-  let l2CacheEnabled = false;
-  let numberOfLines = 64;
+  let matrix1Enabled = false;
+  let matrix2Enabled = false;
+  let matrixEnabled = false;
+  let numberOfLines = 32;
   let address;
   console.log();
+  l1cache = l1cache;
+  let i1 = 0;
+  let j1 = -1;
+  let i2 = -1;
+  let j2 = 0;
+  let matrix1 = new Array(32).fill(null).map(() => new Array(32).fill(null));
+  console.log(matrix1);
+  let matrix2 = new Array(32).fill(null).map(() => new Array(32).fill(null));
+  console.log(matrix2);
+
+  let finalMatrix = new Array(32).fill(null).map(() => new Array(32).fill(null));
+  function matrixMultiplication() {
+    finalMatrix = new Array(32).fill(null).map(() => new Array(32).fill(null));
+    for (let i = 0; i < 32; i++) {
+      for (let j = 0; j < 32; j++) {
+        for (let k = 0; k < 32; k++) {
+          finalMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+        }
+      }
+    }
+  }
+
+  function matrixNextItemCalculation() {
+    if (j1 < 31) {
+      j1++;
+      address = i1 * 32 + j1;
+      let data = l1cache.readCache(address, mainMemory).data;
+      console.log(data, i1, j1, address);
+      matrix1[i1][j1] = data;
+    } else {
+      j1 = -1;
+      i1++;
+    }
+
+    if (i2 < 31) {
+      i2++;
+      address = 1024 + i2 * 32 + j2;
+      matrix2[i2][j2] = l1cache.readCache(address, mainMemory).data;
+    } else {
+      i2 = -1;
+      j2++;
+      matrixMultiplication();
+    }
+  }
+
+  function matrixNextRowCalculation() {
+    while (j1 < 31) {
+      if (j1 < 31) {
+        j1++;
+        address = i1 * 32 + j1;
+        matrix1[i1][j1] = l1cache.readCache(address, mainMemory).data;
+      } else {
+        j1 = -1;
+        i1++;
+      }
+    }
+    j1 = -1;
+    i1++;
+
+    while (i2 < 31) {
+      if (i2 < 31) {
+        i2++;
+        address = 1024 + i2 * 32 + j2;
+        matrix2[i2][j2] = l1cache.readCache(address, mainMemory).data;
+      } else {
+        i2 = -1;
+        j2++;
+      }
+    }
+    i2 = -1;
+    j2++;
+    matrixMultiplication();
+  }
+  mainMemory = mainMemory;
 </script>
 
 <div class="w-screen h-screen flex flex-col items-center">
@@ -34,9 +101,10 @@
       on:click={() => {
         mainMemoryEnabled = true;
         l1cacheEnabled = false;
-        l2CacheEnabled = false;
-        victimCacheEnabled = false;
         numberOfLines = mainMemory.memory.length;
+        matrix1Enabled = false;
+        matrix2Enabled = false;
+        matrixEnabled = false;
       }}>main Memory</button
     >
     <button
@@ -44,9 +112,10 @@
       on:click={() => {
         mainMemoryEnabled = false;
         l1cacheEnabled = true;
-        l2CacheEnabled = false;
-        victimCacheEnabled = false;
         numberOfLines = l1cache.cache.length;
+        matrix1Enabled = false;
+        matrix2Enabled = false;
+        matrixEnabled = false;
       }}>L1 cache</button
     >
     <button
@@ -54,46 +123,49 @@
       on:click={() => {
         mainMemoryEnabled = false;
         l1cacheEnabled = false;
-        l2CacheEnabled = false;
-        victimCacheEnabled = true;
-        numberOfLines = l1cache.victimCache.cache.length;
-      }}>victim Cache</button
+        matrix1Enabled = true;
+        matrix2Enabled = false;
+        matrixEnabled = false;
+      }}>Matrix1</button
     >
     <button
       class="m-2 w-[100px]"
       on:click={() => {
         mainMemoryEnabled = false;
         l1cacheEnabled = false;
-        l2CacheEnabled = true;
-        victimCacheEnabled = false;
-        numberOfLines = l2cache.cache.length;
-      }}>L2 Cache</button
+        matrix1Enabled = false;
+        matrix2Enabled = true;
+        matrixEnabled = false;
+      }}>Matrix2</button
     >
-    <input
-      class="m-2 w-[250px] p-4 rounded-md"
-      type="search"
-      name="prompt"
-      bind:value={address}
-      placeholder="Enter an address in hex or number"
-      required
-    />
     <button
       class="m-2 w-[100px]"
       on:click={() => {
-        let a = l1cache.readCache(address, mainMemory, l2cache);
-        console.log(a);
-        console.log(a);
-        console.log(a);
-        console.log(a);
-        console.log(a);
-      }}>Find</button
+        mainMemoryEnabled = false;
+        l1cacheEnabled = false;
+        matrix1Enabled = false;
+        matrix2Enabled = false;
+        matrixEnabled = true;
+      }}>Final Matrix</button
+    >
+    <button
+      class="m-2 w-[100px]"
+      on:click={() => {
+        matrixNextItemCalculation();
+      }}>read element</button
+    >
+    <button
+      class="m-2 w-[100px]"
+      on:click={() => {
+        matrixNextRowCalculation();
+      }}>read Row</button
     >
   </div>
   <div
     class="overflow-auto m-2 w-4/5 h-4/5 bg-gray-900 rounded-md flex flex-row relative"
   >
     <div
-      class="w-full h-full bg-gray-900 rounded-md grid grid-cols-64 overflow-style-none absolute top-[50px] left-[48px] rounded-tl-none"
+      class="w-full h-full bg-gray-900 rounded-md grid grid-cols-32 overflow-style-none absolute top-[50px] left-[48px] rounded-tl-none"
     >
       <div class="m-1 bg-gray-900 rounded-md">
         {#if l1cacheEnabled}
@@ -117,15 +189,57 @@
             </div>
           {/each}
         {/if}
-        {#if l2CacheEnabled}
-          {#each l2cache.cache as line}
+        {#if matrixEnabled}
+          {#each finalMatrix as line}
             <div class="flex flex-row">
               {#each line as cell}
-                {#if cell[0] != null}
+                {#if cell != null}
                   <div
                     class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
                   >
-                    {cell[0]}
+                    {cell}
+                  </div>
+                {:else}
+                  <div
+                    class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
+                  >
+                    {0}
+                  </div>
+                {/if}
+              {/each}
+            </div>
+          {/each}
+        {/if}
+        {#if matrix1Enabled}
+          {#each matrix1 as line}
+            <div class="flex flex-row">
+              {#each line as cell}
+                {#if cell != null}
+                  <div
+                    class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
+                  >
+                    {cell}
+                  </div>
+                {:else}
+                  <div
+                    class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
+                  >
+                    {0}
+                  </div>
+                {/if}
+              {/each}
+            </div>
+          {/each}
+        {/if}
+        {#if matrix2Enabled}
+          {#each matrix2 as line}
+            <div class="flex flex-row">
+              {#each line as cell}
+                {#if cell != null}
+                  <div
+                    class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
+                  >
+                    {cell}
                   </div>
                 {:else}
                   <div
@@ -159,27 +273,6 @@
             </div>
           {/each}
         {/if}
-        {#if victimCacheEnabled}
-          {#each l1cache.victimCache.cache as line}
-            <div class="flex flex-row">
-              {#each line as cell}
-                {#if cell != null}
-                  <div
-                    class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
-                  >
-                    {cell[0]}
-                  </div>
-                {:else}
-                  <div
-                    class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
-                  >
-                    {0}
-                  </div>
-                {/if}
-              {/each}
-            </div>
-          {/each}
-        {/if}
       </div>
     </div>
     <div
@@ -196,7 +289,7 @@
     <div
       class="bg-gray-900 w- full flex flex-row p-1 border-b-2 h-[50px] sticky top-0"
     >
-      {#each { length: 64 } as _, i}
+      {#each { length: 32 } as _, i}
         <div
           class="m-1 bg-gray-800 p-1 w-[30px] h-[30px] flex items-center justify-center"
         >
--- src/l1cache.js
+++ src/l1cache.js
@@ -16,123 +16,36 @@ class L1cache {
                 this.cache[i][j][1] = null; // tag
             }
         }
-
-        this.victimCache = new victimCache(lineSize, 4);
     }
 
-    readCache(address, mainMemory, l2Cache) {
+    readCache(address, mainMemory) {
         let offset = address % this.lineSize;
         let lineNo = Math.floor(address / this.lineSize) % this.noOfLinesOfCache;
         let tag = Math.floor(address / (this.lineSize * this.noOfLinesOfCache));
         console.log(offset, lineNo, tag);
         let hit = false
         let data = null
-
-        // if the location is empty
         if (this.cache[lineNo][offset][0] == null) {
             let block = mainMemory.readMemory(address);
-            let result = l2Cache.readCache(address, mainMemory);
-            console.log(result);
-            if (result.hit == true) {
-                for (let i = 0; i < this.lineSize; i++) {
-                    this.cache[lineNo][i][0] = result.line[i][0];
-                    this.cache[lineNo][i][1] = tag;
-                }
+            for (let i = 0; i < this.lineSize; i++) {
+                this.cache[lineNo][i][0] = block[i];
+                this.cache[lineNo][i][1] = tag;
+            }
+            data = this.cache[lineNo][offset][0];
+        } else {
+            if (this.cache[lineNo][offset][1] == tag) {
+                data = this.cache[lineNo][offset][0];
+                hit = true;
             } else {
+                let block = mainMemory.readMemory(address);
                 for (let i = 0; i < this.lineSize; i++) {
                     this.cache[lineNo][i][0] = block[i];
                     this.cache[lineNo][i][1] = tag;
                 }
-            }
-            this.victimCache.addLine(this.cache[lineNo], address);
-            data = this.cache[lineNo][offset][0];
-            // if the location if not empty
-        } else {
-            // the location contains the data we need
-            if (this.cache[lineNo][offset][1] == tag) {
-                data = this.cache[lineNo][offset][0];
-                hit = true;
-                // if the location does not contain the data we need
-            } else {
-                let block = mainMemory.readMemory(address);
-                let line1 = this.cache[lineNo];
-                // code for insert the current cache contents into the victim cache
-                let result = l2Cache.readCache(address, mainMemory);
-                console.log(result);
-                if (result.hit == true) {
-                    for (let i = 0; i < this.lineSize; i++) {
-                        this.cache[lineNo][i][0] = result.line[i][0];
-                        this.cache[lineNo][i][1] = tag;
-                    }
-                } else {
-                    let line = this.victimCache.readLine(address)
-                    console.log(line);
-                    if (line != undefined) {
-                        for (let i = 0; i < this.lineSize; i++) {
-                            this.cache[lineNo][i][0] = line[i][0];
-                            this.cache[lineNo][i][1] = line[i][1];
-                        }
-                    } else {
-                        let result = l2Cache.readCache(address, mainMemory);
-                        if (result.hit == true) {
-                            for (let i = 0; i < this.lineSize; i++) {
-                                this.cache[lineNo][i][0] = result.line[i][0];
-                                this.cache[lineNo][i][1] = tag;
-                            }
-                        } else {
-                            for (let i = 0; i < this.lineSize; i++) {
-                                this.cache[lineNo][i][0] = block[i];
-                                this.cache[lineNo][i][1] = tag;
-                            }
-                        }
-                    }
-                }
-                this.victimCache.addLine(line1, address);
                 data = this.cache[lineNo][offset][0];
             }
         }
         return { "data": data, "hit": hit }
-    }
-
-}
-
-class victimCache {
-    constructor(lineSize, noOfLinesOfCache) {
-        this.cache = new Array(noOfLinesOfCache);
-        this.lineSize = lineSize;
-        this.noOfLinesOfCache = noOfLinesOfCache;
-        for (let i = 0; i < noOfLinesOfCache; i++) {
-            this.cache[i] = new Array(lineSize);
-            for (let j = 0; j < lineSize; j++) {
-                this.cache[i][j] = new Array(3);
-                this.cache[i][j][0] = null; // data
-                this.cache[i][j][1] = null; // tag
-                this.cache[i][j][2] = null; // address
-            }
-        }
-
-        this.currentLine = 0;
-    }
-
-    readLine(address) {
-        for (let i = 0; i < this.noOfLinesOfCache; i++) {
-            if (this.cache[i][2] == address) {
-                return this.cache[i];
-            }
-        }
-    }
-
-    addLine(line, address) {
-        console.log(this.readLine(address))
-        if (this.readLine(address) == undefined) {
-            let initialAddress = address - (address % this.lineSize)
-            for (let i = 0; i < this.lineSize; i++) {
-                this.cache[this.currentLine][i][0] = line[i][0];
-                this.cache[this.currentLine][i][1] = line[i][1];
-                this.cache[this.currentLine][i][2] = initialAddress + i;
-            }
-            this.currentLine = (this.currentLine + 1) % this.noOfLinesOfCache;
-        }
     }
 
 }
